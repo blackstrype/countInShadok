46cf3009ca07c356a6ae78bcbc2fb84b30fed625
Scott MESSNER <smessner@clever-age.com>
2023-10-23 12:43:23 +0200

    First implementation of NumberInShadok.

--- src/main/java/com/blackstrype/shadok/NumberInShadok.java
+++ src/main/java/com/blackstrype/shadok/NumberInShadok.java
@@ -1,4 +1,24 @@
 package com.blackstrype.shadok;
 
+import java.util.stream.Collectors;
+
 public class NumberInShadok {
+    public static String shadok(int number) {
+        String gabuzomeu;
+        String numBase4 = Integer.toString(number, 4);
+        gabuzomeu = numBase4.chars()
+                .mapToObj((digit) -> {
+                    if (digit == '0') {
+                        return "GA";
+                    } else if (digit == '1') {
+                        return "BU";
+                    } else if (digit == '2') {
+                        return "ZO";
+                    } else {
+                        return "MEU";
+                    }
+                })
+                .collect(Collectors.joining());
+        return gabuzomeu;
+    }
 }
