6bdc26e150819a94e25e346e5529d59d82e8b3fd
Scott MESSNER <smessner@clever-age.com>
2023-10-23 12:09:13 +0200

    Remove tests -- They need to be written TDD-style

--- src/test/java/com/blackstrype/shadok/TestNumberInShadok.java
+++ src/test/java/com/blackstrype/shadok/TestNumberInShadok.java
@@ -1,8 +1,5 @@
 package com.blackstrype.shadok;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class TestNumberInShadok {
     /*
      0 = GA     4 = BUGA    8  = ZOGA   12 = MEUGA      16 = BUGAGA
@@ -10,42 +7,4 @@ public class TestNumberInShadok {
      2 = ZO     6 = BUZO    10 = ZOZO   14 = MEUZO      18 = BUGAZO
      3 = MEU    7 = BUMEU   11 = ZOMEU  15 = MEUMEU     19 = BUGAMEU
     */
-
-    @Test
-    public void ZeroShouldBeGA(){
-        Assertions.assertEquals(NumberInShadok.shadok(0), "GA");
-    }
-    @Test
-    public void OneShouldBeBU() {
-        Assertions.assertEquals(NumberInShadok.shadok(1), "BU");
-    }
-    @Test
-    public void TwoShouldBeZO() {
-        Assertions.assertEquals(NumberInShadok.shadok(2), "ZO");
-    }
-    @Test
-    public void ThreeShouldBeMEU() {
-        Assertions.assertEquals(NumberInShadok.shadok(3), "MEU");
-    }
-    @Test
-    public void FourShouldBeBUGA() {
-        Assertions.assertEquals(NumberInShadok.shadok(4), "BUGA");
-    }
-    @Test
-    public void FiveShouldBeBUBU() {
-        Assertions.assertEquals(NumberInShadok.shadok(5), "BUBU");
-    }
-    @Test
-    public void EightShouldBeBUBU() throws Exception {
-        Assertions.assertEquals(NumberInShadok.shadok(8), "ZOGA");
-    }
-    @Test
-    public void SixteenShouldBeBUGAGA() throws Exception {
-        Assertions.assertEquals(NumberInShadok.shadok(16), "BUGAGA");
-    }
-    @Test
-    public void SixtyFourShouldBeBUGAGA() throws Exception {
-        Assertions.assertEquals(NumberInShadok.shadok(64), "BUGAGAGA");
-    }
-
 }
