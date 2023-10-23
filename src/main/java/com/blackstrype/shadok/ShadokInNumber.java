84a3db0d1806cf23827a32a3d71622cd1c9dcad2
Scott MESSNER <smessner@clever-age.com>
2023-10-23 12:52:10 +0200

    First implementation of ShadokInNumber.

--- src/main/java/com/blackstrype/shadok/ShadokInNumber.java
+++ src/main/java/com/blackstrype/shadok/ShadokInNumber.java
@@ -1,4 +1,12 @@
 package com.blackstrype.shadok;
 
 public class ShadokInNumber {
+    public static int shadok(String gabuzomeu) {
+        String base4Number = gabuzomeu
+                .replaceAll("GA", "0")
+                .replaceAll("BU", "1")
+                .replaceAll("ZO", "2")
+                .replaceAll("MEU", "3");
+        return Integer.parseInt(base4Number, 4);
+    }
 }
