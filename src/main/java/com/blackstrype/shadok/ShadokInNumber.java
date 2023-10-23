e31fed01b138541b546415ce6b9ddd770c0ee66a
Scott MESSNER <smessner@clever-age.com>
2023-10-23 12:06:17 +0200

    Correct dependencies for test.

---
+++ src/main/java/com/blackstrype/shadok/ShadokInNumber.java
@@ -0,0 +1,4 @@
+package com.blackstrype.shadok;
+
+public class ShadokInNumber {
+}
--- src/test/java/com/blackstrype/shadok/TestShadokInNumber.java
+++ src/test/java/com/blackstrype/shadok/TestShadokInNumber.java
@@ -1,8 +1,7 @@
 package com.blackstrype.shadok;
 
-import org.junit.Assert;
-import org.junit.Test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TestShadokInNumber {
     /*
@@ -13,58 +12,58 @@ public class TestShadokInNumber {
     */
     @Test
     public void GAShouldBeZero(){
-        Assert.assertEquals(ShadokInNumber.shadok("GA"), 0, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("GA"), 0, 0.1);
     }
     @Test
     public void BUShouldBeOne(){
-        Assert.assertEquals(ShadokInNumber.shadok("BU"), 1, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BU"), 1, 0.1);
     }
     @Test
     public void ZOShouldBeTwo(){
-        Assert.assertEquals(ShadokInNumber.shadok("ZO"), 2, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("ZO"), 2, 0.1);
     }
     @Test
     public void MEUShouldBeThree(){
-        Assert.assertEquals(ShadokInNumber.shadok("MEU"), 3, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("MEU"), 3, 0.1);
     }
     @Test
     public void BUGAShouldBeFour(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUGA"), 4, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUGA"), 4, 0.1);
     }
     @Test
     public void BUBUShouldBeFive(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUBU"), 5, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUBU"), 5, 0.1);
     }
     @Test
     public void BUZOShouldBeSix(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUZO"), 6, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUZO"), 6, 0.1);
     }
     @Test
     public void BUMEUShouldBeSeven(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUMEU"), 7, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUMEU"), 7, 0.1);
     }
     @Test
     public void ZOGAShouldBeEight(){
-        Assert.assertEquals(ShadokInNumber.shadok("ZOGA"), 8, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("ZOGA"), 8, 0.1);
     }
     @Test
     public void ZOBUShouldBeNine(){
-        Assert.assertEquals(ShadokInNumber.shadok("ZOBU"), 9, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("ZOBU"), 9, 0.1);
     }
     @Test
     public void MEUGAShouldBeTwelve(){
-        Assert.assertEquals(ShadokInNumber.shadok("MEUGA"), 12, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("MEUGA"), 12, 0.1);
     }
     @Test
     public void MEUMEUShouldBeFifeteen(){
-        Assert.assertEquals(ShadokInNumber.shadok("MEUMEU"), 15, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("MEUMEU"), 15, 0.1);
     }
     @Test
     public void BUGAGAShouldBeSixteen(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUGAGA"), 16, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUGAGA"), 16, 0.1);
     }
     @Test
     public void BUGAGAGAShouldBeSixtyFour(){
-        Assert.assertEquals(ShadokInNumber.shadok("BUGAGAGA"), 64, 0.1);
+        Assertions.assertEquals(ShadokInNumber.shadok("BUGAGAGA"), 64, 0.1);
     }
 }
